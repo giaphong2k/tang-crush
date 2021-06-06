17e87b6f31cedcf303afa7a79259eeeafd28d590
giaphong2k <67235410+giaphong2k@users.noreply.github.com>
2021-06-06 22:57:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Hạnh à!',
+    introDesc: `Trái đất vốn lạ thường <3,
+    Mà sao em cứ đi nhầm đường.
+    Lạc vào tim anh lẻ loi :3,
+    Đằng sau chữ yêu đây là thương!!!`,
+    btnIntro: 'Hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Em yêu anh nhiều lắm ý <3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Hẹn gặp em vào tháng 7 nha, mong sớm lên HN với bé lắm ý.',
     btnAccept: 'Okiiiii lun ',
     messLink: 'https://www.facebook.com/giaphong.ha.12/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
